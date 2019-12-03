9da31f7ed35ddac27488ec18ff43afa8341e7608
Edmund Lau <tmlcyaa@gmail.com>
2019-12-03 18:26:19 +0800

    map initialize

---
+++ pages/map/map.js
@@ -0,0 +1,145 @@
+// pages/map/map.js
+
+// 引入SDK核心类
+var QQMapWX = require('../../libs/qqmap-wx-jssdk.js');
+var qqmapsdk;
+
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+    qqmapsdk = new QQMapWX({
+      key: 'CNSBZ-V5BKX-KO24V-T2TXY-2XVET-YYB6H'
+    });
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+  backfill: function (e) {
+    var id = e.currentTarget.id;
+    for (var i = 0; i < this.data.suggestion.length; i++) {
+      if (i == id) {
+        this.setData({
+          backfill: this.data.suggestion[i].title
+        });
+      }
+    }
+  },
+
+  //触发关键词输入提示事件
+  getsuggest: function (e) {
+    var _this = this;
+    //调用关键词提示接口
+    qqmapsdk.getSuggestion({
+      //获取输入框值并设置keyword参数
+      keyword: e.detail.value, //用户输入的关键词，可设置固定值,如keyword:'KFC'
+      //region:'北京', //设置城市名，限制关键词所示的地域范围，非必填参数
+      success: function (res) {//搜索成功后的回调
+        console.log(res);
+        var sug = [];
+        for (var i = 0; i < res.data.length; i++) {
+          sug.push({ // 获取返回结果，放到sug数组中
+            title: res.data[i].title,
+            id: res.data[i].id,
+            addr: res.data[i].address,
+            city: res.data[i].city,
+            district: res.data[i].district,
+            latitude: res.data[i].location.lat,
+            longitude: res.data[i].location.lng
+          });
+        }
+        _this.setData({ //设置suggestion属性，将关键词搜索结果以列表形式展示
+          suggestion: sug
+        });
+      },
+      fail: function (error) {
+        console.error(error);
+      },
+      complete: function (res) {
+        console.log(res);
+      }
+    });
+  },
+  /**
+   * Lifecycle function--Called when page show
+   */
+
+  formSubmit: function () {
+    let plugin = requirePlugin('routePlan');
+    let key = 'CNSBZ-V5BKX-KO24V-T2TXY-2XVET-YYB6H';  //使用在腾讯位置服务申请的key
+    let referer = 'curiocity用户端';   //调用插件的app的名称
+    let endPoint = JSON.stringify({  //终点
+      'name': '吉野家(北京西站北口店)',
+      'latitude': 39.89631551,
+      'longitude': 116.323459711
+    });
+    wx.navigateTo({
+      url: 'plugin://routePlan/index?key=' + key + '&referer=' + referer + '&endPoint=' + endPoint
+    });
+  },
+
+  onShow: function () {
+    // 调用接口
+    qqmapsdk.search({
+      keyword: 'Majestic',
+      success: function (res) {
+        console.log(res);
+      },
+      fail: function (res) {
+        console.log(res);
+      },
+      complete: function (res) {
+        console.log(res);
+      }
+    });
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
