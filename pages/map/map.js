a659975f17e806e5d62a57b7191d2cbc9d206516
Edmund Lau <tmlcyaa@gmail.com>
2019-12-04 11:12:14 +0800

    add walking logic

--- pages/map/map.js
+++ pages/map/map.js
@@ -39,31 +39,42 @@ Page({
     }
   },
 
-  //触发关键词输入提示事件
-  getsuggest: function (e) {
+  /**
+   * Lifecycle function--Called when page show
+   */
+
+  formSubmit(e) {
     var _this = this;
-    //调用关键词提示接口
-    qqmapsdk.getSuggestion({
-      //获取输入框值并设置keyword参数
-      keyword: e.detail.value, //用户输入的关键词，可设置固定值,如keyword:'KFC'
-      //region:'北京', //设置城市名，限制关键词所示的地域范围，非必填参数
-      success: function (res) {//搜索成功后的回调
+    //调用距离计算接口
+    qqmapsdk.direction({
+      mode: 'walking',//可选值：'driving'（驾车）、'walking'（步行）、'bicycling'（骑行），不填默认：'driving',可不填
+      //from参数不填默认当前地址
+      from: e.detail.value.start,
+      to: e.detail.value.dest,
+      success: function (res) {
         console.log(res);
-        var sug = [];
-        for (var i = 0; i < res.data.length; i++) {
-          sug.push({ // 获取返回结果，放到sug数组中
-            title: res.data[i].title,
-            id: res.data[i].id,
-            addr: res.data[i].address,
-            city: res.data[i].city,
-            district: res.data[i].district,
-            latitude: res.data[i].location.lat,
-            longitude: res.data[i].location.lng
-          });
+        var ret = res;
+        var coors = ret.result.routes[0].polyline, pl = [];
+        //坐标解压（返回的点串坐标，通过前向差分进行压缩）
+        var kr = 1000000;
+        for (var i = 2; i < coors.length; i++) {
+          coors[i] = Number(coors[i - 2]) + Number(coors[i]) / kr;
         }
-        _this.setData({ //设置suggestion属性，将关键词搜索结果以列表形式展示
-          suggestion: sug
-        });
+        //将解压后的坐标放入点串数组pl中
+        for (var i = 0; i < coors.length; i += 2) {
+          pl.push({ latitude: coors[i], longitude: coors[i + 1] })
+        }
+        console.log(pl)
+        //设置polyline属性，将路线显示出来,将解压坐标第一个数据作为起点
+        _this.setData({
+          latitude: pl[0].latitude,
+          longitude: pl[0].longitude,
+          polyline: [{
+            points: pl,
+            color: '#FF0000DD',
+            width: 4
+          }]
+        })
       },
       fail: function (error) {
         console.error(error);
@@ -71,23 +82,6 @@ Page({
       complete: function (res) {
         console.log(res);
       }
-    });
-  },
-  /**
-   * Lifecycle function--Called when page show
-   */
-
-  formSubmit: function () {
-    let plugin = requirePlugin('routePlan');
-    let key = 'CNSBZ-V5BKX-KO24V-T2TXY-2XVET-YYB6H';  //使用在腾讯位置服务申请的key
-    let referer = 'curiocity用户端';   //调用插件的app的名称
-    let endPoint = JSON.stringify({  //终点
-      'name': '吉野家(北京西站北口店)',
-      'latitude': 39.89631551,
-      'longitude': 116.323459711
-    });
-    wx.navigateTo({
-      url: 'plugin://routePlan/index?key=' + key + '&referer=' + referer + '&endPoint=' + endPoint
     });
   },
 
